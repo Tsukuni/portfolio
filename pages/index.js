c1b6956afd4039f54c363cc1e66c30ff00f02f35
Tsukuni <tamkchi.fugu@gmail.com>
2019-04-30 23:29:58 +0900

    fontawesome追加

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import "../static/scss/styles.scss";
 import icon from "../static/images/icon.jpg";
 import BusinessCard from  "../components/BusinessCard";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare, faTwitterSquare, faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Index = () => (
   <div className="top">
     <BusinessCard>
       <img src={ icon }/>
       <h1>Sample</h1>
+      <FontAwesomeIcon icon={faGithubSquare} />
+      <FontAwesomeIcon icon={faTwitterSquare} />
+      <FontAwesomeIcon icon={faFacebookSquare} />
+      <FontAwesomeIcon icon={faInstagram} />
     </BusinessCard>
   </div>
 )
