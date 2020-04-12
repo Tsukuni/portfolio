2c58eb3533079f57af104ca5d7ecbde9e0888c05
Tsukuni <tamkchi.fugu@gmail.com>
2020-04-12 23:45:54 +0900

    remove react-scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { Reset } from 'styled-reset';
 import FirstPage from './pages/FirstPage';
 import SecondPage from './pages/SecondPage';
 import Header from './components/Header';
 
-const App: React.FC<{}> = () => (
-  <Container>
-    <Reset />
-    <Header />
-    <FirstPage />
-    <SecondPage />
-  </Container>
-);
+const App: React.FC<{}> = () => {
+  return (
+    <Container>
+      <Reset />
+      <Header />
+      <FirstPage />
+      <SecondPage />
+    </Container>
+  );
+};
 
 const Container = styled.div`
   font-family: 'Noto Sans JP', sans-serif;
   font-family: 'Lexend Exa', sans-serif;
-  overflow: hidden;
   background-color: rgb(19, 59, 28);
 `;
 
