f2d87ad99639ad0d115354ba0a4de84626fbcabf
Tsukuni <tamkchi.fugu@gmail.com>
2020-04-12 19:37:39 +0900

    Container置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Reset } from 'styled-reset';
-import TopContainer from './containers/TopContainer';
+import FirstPage from './components/FirstPage';
+import Header from './components/Header';
 
-const App = () => (
-  <Wrapper>
+const App: React.FC<{}> = () => (
+  <Container>
     <Reset />
-    <TopContainer />
-  </Wrapper>
+    <Header />
+    <FirstPage />
+  </Container>
 );
 
-const Wrapper = styled.div`
+const Container = styled.div`
   font-family: 'Noto Sans JP', sans-serif;
   font-family: 'Lexend Exa', sans-serif;
   overflow: hidden;
+  background-color: rgb(19, 59, 28);
 `;
 
 export default App;
---
+++ src/components/FirstPage/FirstPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Container from '../Container';
+import ProfileCard from '../ProfileCard';
+
+const FirstPage: React.FC<{}> = () => (
+  <Container>
+    <ProfileCard />
+  </Container>
+);
+
+export default FirstPage;
